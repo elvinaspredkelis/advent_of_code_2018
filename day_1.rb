d8471e0be2a53a62e68d6979a76aa251a4fde163
Elvinas Predkelis <elvinasnet@gmail.com>
2018-12-04 09:18:03 +0100

    Just a little contribution.

--- day_1.rb
+++ day_1.rb
@@ -1,7 +1,7 @@
 sum = 0
 File.open("inputs/input_day_1.txt", "r") do |f|
   f.each_line do |line|
-   line[0] == '+' ? sum += line[1..-1].to_i : sum -=l ine[1..-1].to_i
+    line[0] == '+' ? sum += line[1..-1].to_i : sum -= line[1..-1].to_i
   end
 end
 puts sum
