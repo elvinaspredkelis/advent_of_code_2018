a6aa381203730655cd87a18983d503efc0add138
Elvinas Predkelis <elvinasnet@gmail.com>
2018-12-04 14:20:21 +0100

    Just a little contribution.

--- day_2.rb
+++ day_2.rb
@@ -14,3 +14,24 @@ def star_1
   twos * threes
 end
 puts "First solution: #{star_1}"
+
+def star_2
+  crates = File.open('inputs/input_day_2.txt').read.chomp.split("\n")
+  crates.each_with_index do |first_crate, i|
+    (i + 1..crates.length - 1).each do |j|
+      second_crate = crates[j]
+      delta = 0
+
+      first_crate.each_char.with_index do |char, k|
+        delta += 1 unless char == second_crate.chars[k]
+      end
+
+      base = second_crate.split(//)
+      subtraction = first_crate.split(//)
+      difference = (base - (base - subtraction)).join
+
+      return difference if delta == 1
+    end
+  end
+end
+puts "Second solution: #{star_2}"
